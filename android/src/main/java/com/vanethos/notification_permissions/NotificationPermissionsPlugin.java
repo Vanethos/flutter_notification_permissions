618502e4daa6ca5b3c745d6041301e733c6cd65f
Sebastian Roth <sebastian.roth@gmail.com>
2019-04-18 14:18:44 +0800

    Replace Kotlin with Java implementation for easier project maintenance.

---
+++ android/src/main/java/com/vanethos/notification_permissions/NotificationPermissionsPlugin.java
@@ -0,0 +1,60 @@
+package com.vanethos.notification_permissions;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+
+import androidx.core.app.NotificationManagerCompat;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+public class NotificationPermissionsPlugin implements MethodChannel.MethodCallHandler {
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel =
+        new MethodChannel(registrar.messenger(), "notification_permissions");
+    channel.setMethodCallHandler(new NotificationPermissionsPlugin(registrar));
+  }
+
+  private static final String PERMISSION_GRANTED = "granted";
+  private static final String PERMISSION_DENIED = "denied";
+
+  private final Context context;
+
+  private NotificationPermissionsPlugin(Registrar registrar) {
+    this.context = registrar.context();
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+    if ("getNotificationPermissionStatus".equalsIgnoreCase(call.method)) {
+      result.success(getNotificationPermissionStatus());
+    } else if ("requestNotificationPermissions".equalsIgnoreCase(call.method)) {
+      if (PERMISSION_DENIED.equalsIgnoreCase(getNotificationPermissionStatus())) {
+        if (context instanceof Activity) {
+          final Activity activity = (Activity) this.context;
+          final Intent intent = new Intent(Settings.ACTION_APPLICATION_SETTINGS);
+          final Uri uri = Uri.fromParts("package", activity.getPackageName(), null);
+
+          intent.setData(uri);
+
+          activity.startActivity(intent);
+        } else {
+          result.error(call.method, "context is not instance of Activity", null);
+        }
+      } else {
+        result.success(null);
+      }
+    } else {
+      result.notImplemented();
+    }
+  }
+
+  private String getNotificationPermissionStatus() {
+    return (NotificationManagerCompat.from(context).areNotificationsEnabled())
+        ? PERMISSION_GRANTED
+        : PERMISSION_DENIED;
+  }
+}
